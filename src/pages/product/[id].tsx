a436685b1ecc3e084f2a671eee3fa05b55163dc9
Davi Pessoa <davialvespessoa@gmail.com>
2024-08-22 21:59:09 -0300

    Estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,8 +1,29 @@
-import { useRouter } from "next/router"
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "@/src/styles/pages/product";
+import { useRouter } from "next/router";
 
 export default function Product() {
-  const { query } = useRouter()
+  const { query } = useRouter();
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
-  )
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias
+          cupiditate tenetur quos dolorem modi dolores incidunt nemo deserunt
+          dolor, quia alias quas minus excepturi officiis, numquam quam
+          accusamus possimus at!
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
