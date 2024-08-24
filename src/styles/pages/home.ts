1bb2395c51dbe63ac897b195a960c9e2d062243a
Davi Pessoa <davialvespessoa@gmail.com>
2024-08-24 20:27:21 -0300

    Prefetch de links

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -9,11 +9,11 @@ export const HomeContainer = styled("main", {
   minHeight: 656,
 });
 
-export const Product = styled("a", {
+export const Product = styled("div", {
   background: "linear-gradient(180deg, #1ea483 0%, #7465d4 100%)",
   borderRadius: 8,
-  // padding: '0.25rem',
-  cursor: "pointer",
+  // padding: "0.25rem",
+  // cursor: "pointer",
   position: "relative",
   overflow: "hidden",
 
