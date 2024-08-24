ee6b8a7beb1f2c0e72a41294847be1e6bedf3d19
Davi Pessoa <davialvespessoa@gmail.com>
2024-08-24 19:27:59 -0300

    SSG com parâmetro dinâmico

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -4,7 +4,7 @@ import {
   ProductContainer,
   ProductDetails,
 } from "@/src/styles/pages/product";
-import { GetStaticProps } from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import Stripe from "stripe";
@@ -39,6 +39,13 @@ export default function Product({ product }: ProductProps) {
     </ProductContainer>
   );
 }
+
+export const getStaticPaths: GetStaticPaths = () => {
+  return {
+    paths: [{ params: { id: "prod_Qe4b6Wm7mTfRbv" } }],
+    fallback: false,
+  };
+};
 
 export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
   params,
